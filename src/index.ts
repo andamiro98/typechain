eef487c6e68522b5f6b90f010c8c96ef5043aa49
andamiro98 <rlatmdwo9@gmail.com>
2023-04-28 18:30:24 +0900

    import crypto and make hash

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,25 @@
-console.log("bye")
+import crypto from "crypto"
+
+interface BolckShape{
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
+class Block implements BolckShape{
+  public hash: string;
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data : string
+  ){
+    this.hash = Block.calculateHash(prevHash, height, data);
+
+  }
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height}${data}`
+
+  }
+
+}
 
